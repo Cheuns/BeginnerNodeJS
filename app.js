787628dff7f2daa4cbd0d896740d2e1210ea4e03
Cheuns <guillaume55260@gmail.com>
2020-10-13 15:12:45 +0200

    NodeJS_v2

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-let stuff = require('./stuff')
+let events = require('events');
+let util = require('util');
 
-console.log(stuff.counter(['shaun','crystal','ryu']));
-console.log(stuff.adder(5,6));
-console.log(stuff.adder(stuff.pi, 5));
+let Person = function(name){
+    this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+let james = new Person('james');
+let mary = new Person('mary');
+let ryan = new Person('ryan');
+
+let people = [james, mary, ryan];
+
+people.forEach(function(person){
+    person.on('speak', function(msg){
+        console.log(person.name + ' said: ' + msg);
+    });
+});
+
+james.emit('speak', 'Hey dudes im back');
+ryan.emit('speak', 'I want a Hamburger');
+mary.emit('speak', 'I love Badminton');
